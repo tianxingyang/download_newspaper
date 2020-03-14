898d13a944ac19665231e4cd4bc4e0617e321176
tianxingyang <1586608030@qq.com>
2020-03-14 14:46:47 +0800

    增加logger的封装，现在可以正确记录日志发生的行数了

--- download_globaltimes.py
+++ download_globaltimes.py
@@ -22,7 +22,49 @@ has_serverchan = False
 last_download_date = datetime.date.fromtimestamp(0)
 
 
-class Logger():
+class LoggerWrapper(logging.Logger):
+    def __init__(self, name, level):
+        super().__init__(name, level)
+
+    def findCaller(self, stack_info=False, stacklevel=1):
+        """
+        Find the stack frame of the caller so that we can note the source
+        file name, line number and function name.
+        """
+        f = logging.currentframe()
+        # On some versions of IronPython, currentframe() returns None if
+        # IronPython isn't run with -X:Frames.
+        if f is not None:
+            # 这里多找一次，因为代码中对 logging 做了一次封装
+            f = f.f_back.f_back
+        orig_f = f
+        while f and stacklevel > 1:
+            f = f.f_back
+            stacklevel -= 1
+        if not f:
+            f = orig_f
+        rv = "(unknown file)", 0, "(unknown function)", None
+        while hasattr(f, "f_code"):
+            co = f.f_code
+            filename = os.path.normcase(co.co_filename)
+            if filename == logging._srcfile:
+                f = f.f_back
+                continue
+            sinfo = None
+            if stack_info:
+                sio = io.StringIO()
+                sio.write('Stack (most recent call last):\n')
+                traceback.print_stack(f, file=sio)
+                sinfo = sio.getvalue()
+                if sinfo[-1] == '\n':
+                    sinfo = sinfo[:-1]
+                sio.close()
+            rv = (co.co_filename, f.f_lineno, co.co_name, sinfo)
+            break
+        return rv
+
+
+class LoggingWrapper():
     _instance = None
 
     def __new__(cls):
@@ -31,24 +73,21 @@ class Logger():
             formatter = logging.Formatter(
                 '%(asctime)s %(filename)s[line:%(lineno)d] %(message)s')
             # 初始化 info logger
-            cls._info_logger = logging.getLogger("info_logger")
-            cls._info_logger.setLevel(logging.INFO)
+            cls._info_logger = LoggerWrapper("info_log", logging.INFO)
             fh_info = logging.FileHandler("info.log")
             fh_info.setLevel(logging.INFO)
             fh_info.setFormatter(formatter)
             cls._info_logger.addHandler(fh_info)
 
             # 初始化 debug logger
-            cls._debug_logger = logging.getLogger("debug_logger")
-            cls._debug_logger.setLevel(logging.DEBUG)
+            cls._debug_logger = LoggerWrapper("debug_log", logging.DEBUG)
             fh_debug = logging.FileHandler("debug.log")
             fh_debug.setLevel(logging.DEBUG)
             fh_debug.setFormatter(formatter)
             cls._debug_logger.addHandler(fh_debug)
 
             # 初始化 error logger
-            cls._error_logger = logging.getLogger("error_logger")
-            cls._error_logger.setLevel(logging.ERROR)
+            cls._error_logger = LoggerWrapper("error_log", logging.ERROR)
             fh_error = logging.FileHandler("error.log")
             fh_error.setLevel(logging.ERROR)
             fh_error.setFormatter(formatter)
@@ -57,16 +96,16 @@ class Logger():
         return cls._instance
 
 
-def log_run(info):
-    Logger()._info_logger.info(info)
+def log_run(log_content):
+    LoggingWrapper()._info_logger.info(log_content)
 
 
-def log_debug(info):
-    Logger()._debug_logger.debug(info)
+def log_debug(log_content):
+    LoggingWrapper()._debug_logger.debug(log_content)
 
 
-def log_error(info):
-    Logger()._error_logger.error(info)
+def log_error(log_content):
+    LoggingWrapper()._error_logger.error(log_content)
 
 
 def serverchan_send(text="", desp=""):
